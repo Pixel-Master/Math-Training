8507e5e632820151367b3ad7be5eb1dd832e1f9b
Pixel-Master <102541954+Pixel-Master@users.noreply.github.com>
2022-04-11 18:14:11 +0200

    Update WindowVariant.py

--- WindowVariant.py
+++ WindowVariant.py
@@ -2,13 +2,13 @@
 
 # Imports
 import tkinter as tk
+from tkinter import messagebox as msg
 import random as rnd
 
 # The Launcher Window
 root = tk.Tk()
-root.title('Math Training Launcher.0.1')
+root.title('Math Training Launcher v0.2')
 root.resizable(False, False)
-root.iconbitmap('/Users/constipixel/Documents/Phyton/math_quiz/assets/math_quiz_icon.ico')
 root.geometry('600x400+50+50')
 
 # Setting up the Variables
@@ -21,7 +21,6 @@ def settings():
     settingswindow = tk.Tk()
     settingswindow.title('Settings')
     settingswindow.resizable(False, False)
-    settingswindow.iconbitmap('/Users/constipixel/Documents/Phyton/math_quiz/assets/math_quiz_icon.ico')
     settingswindow.geometry('600x400+50+50')
     Close = tk.Button(settingswindow, text="Close", command=settingswindow.destroy, fg="red")
     Close.pack(side="bottom")
@@ -36,34 +35,65 @@ global started
 global hello
 started = tk.Button(root, text='Next', fg="black", font=("Arial", 30))
 started.pack(side="bottom")
-hello = tk.Label(root, text="WELCOME TO THE MATH QUIZ!(version: 0.1)!", font=("Arial", 25, "bold"))
+hello = tk.Label(root, text="WELCOME TO THE MATH QUIZ!", font=("Arial", 25, "bold"))
 hello.pack()
 
 
 # The event for the "Play!" Button
-# noinspection PyTypeChecker
 def startgame():
     Answer = format(value_inside.get())
-    print(Answer)
     print("Selected Option: {}".format(value_inside.get()))
+    root.withdraw()
     # The mainloop for "+"
     if Answer == "+":
+        # Setting up the Window
         plus = tk.Tk()
-        plus.title("Math")
+        plus.title("Plus Math Training v0.2")
         plus.resizable(False, False)
-        plus.iconbitmap('/Users/constipixel/Documents/Phyton/math_quiz/assets/math_quiz_icon.ico')
         plus.geometry('600x400+50+50')
-        a = rnd.randint(1, 5)
-        b = rnd.randint(1, 5)
-        questionLabel = tk.Label(plus, text=("What results in" + a + "+" + b + "?"), font=("Arial", 25, "bold"))
-        questionLabel.pack()
+
+        # Some Stuff
+        def result(score):
+            answ = tk.StringVar()
+            p_a = rnd.randint(1, 5)
+            p_b = rnd.randint(1, 5)
+            plus_answer = p_a + p_b
+            questionLabel = tk.Label(plus, text=("What results in 0 + 0 ?"), font=("Arial", 25, "bold"))
+            questionLabel.pack(side="top")
+            answerField = tk.Entry(plus, width=20,textvariable=answ)
+            answerField.insert(0,"")
+            answerField.pack(side="bottom")
+
+        result(score)
+    # The mainloop for "-"
+    elif Answer == "-":
+        msg.askyesno("dev -", "This is still in development do you want to proceed anyways", )
+        msg.showerror("nothing -", "This Mode is still in developing")
+
+    # The mainloop for "*"
+    elif Answer == "*":
+        msg.askyesno("dev *", "This is still in development do you want to proceed anyways", )
+        msg.showerror("nothing *", "This Mode is still in developing")
+
+    # The mainloop for "/"
+    elif Answer == "/":
+        msg.askyesno("dev /", "This is still in development do you want to proceed anyways", )
+        msg.showerror("nothing /", "This Mode is still in developing")
+
+    # Error when selecting no Mode
+    elif Answer == "Select a Mode":
+        msg.showwarning("No Mode", "Select a valid Mode")
+
+    # You will NEVER see THIS
+    else:
+        msg.showwarning("Fatal", "You've broke the System!")
 
 
 # The function for the "Select a Mode" table
 def start():
     global value_inside
     value_inside = tk.StringVar(root)
-    options_list = ["+", "-", "*", "-"]
+    options_list = ["+", "-", "*", "/"]
     value_inside.set("Select a Mode")
     hello.config(text="Choose your Mode!")
     question_menu = tk.OptionMenu(root, value_inside, *options_list)
@@ -71,7 +101,8 @@ def start():
     started.config(text="Play!", command=startgame)
 
 
-# Adds a command to the "Next" Button
+# Adds an event to the "Next" Button
 started.config(command=start)
+
 # The mainloop
 root.mainloop()
