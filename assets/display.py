b6f9d6d8924970f1d6ba48cbd24f28ffe264f3a4
Pixel-Master <102541954+Pixel-Master@users.noreply.github.com>
2022-05-04 14:31:44 +0200

    Added a script wich gets your display size

---
+++ assets/display.py
@@ -0,0 +1,6 @@
+
+import tkinter as tk
+root = tk.Tk()
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+print("Your Screen has a Resolution of:",screen_width,"x",screen_height)
